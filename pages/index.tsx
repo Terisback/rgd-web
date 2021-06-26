079774ac42a79eb3eac414a2d571b45cd4a664a5
YellowAfterlife <yellowafterlife@hotmail.com>
2021-06-26 17:03:46 +0300

    Текст для начальной страницы и временный текст для страницы Донатеры

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,33 @@
 export default function Home() {
-  return <div>Игры</div>;
+  return <section>
+    <p>
+    Поздравляем!
+    Вы попали на официальный сайт дискорд сервера для геймдевов.
+    Если вы — крутой прогер, моделлер, дизигнер, художник, аниматор или просто <s>задрот</s> хороший специалист
+    — значит, вы пришли по адресу и можете принять участие в жизни лампового комьюнити,
+    которое точно оценит ваш скилл.
+    </p><p>
+      Присоединится можно нажатием на впечатляюще широкую кнопочку:
+    </p>
+    <a href="https://discord.gg/5kZhhWD" style={{
+      textAlign: "center",
+      backgroundImage: "linear-gradient(97.5deg, #738BD7 calc(50% + 25px), #697EC4 calc(50% + 25px))",
+      lineHeight: 0,
+      display: "block"
+    }}>
+      <img
+        src="https://discordapp.com/api/guilds/504617984594018325/widget.png?style=banner2"
+        alt="Та самая впечатляюще широкая кнопка, на которую нужно нажать, чтобы присоединится к серверу"
+      />
+    </a><p>
+      <b>Примечание:</b> на данный момент сайт находится на стадии разработки, но на сервере всё есть.
+    </p>
+  </section>;
+  /*
+    <iframe
+    src="https://discord.com/widget?id=504617984594018325&theme=dark"
+    width="350" height="500"
+    allowtransparency="true" frameBorder="0"
+    sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
+  */
 }
